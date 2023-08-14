906a9f5d8bcfe7902eae7d0be866a4d6c5e637e3
Óscar <oscarigrexas@gmail.com>
2023-08-14 17:18:55 +0200

    fix: Return to waitress server

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def index():
 
 
 if __name__ == "__main__":
-    # from waitress import serve
+    from waitress import serve
 
-    # serve(app, host="0.0.0.0", port=80)
+    serve(app, host="0.0.0.0", port=80)
 
-    app.run(port=5000)
+    # app.run(port=5000)
